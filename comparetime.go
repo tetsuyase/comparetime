77c739b89f1d890a21400b59dc8c358aff779eda
senahatetsuya <senaha.tetsuya@gmail.com>
2024-09-26 21:56:32 +0900

    add comment

--- comparetime.go
+++ comparetime.go
@@ -2,22 +2,27 @@ package comparetime
 
 import "time"
 
+// a < b
 func lessThanSign(a time.Time, b time.Time) bool {
 	return a.Before(b)
 }
 
+// a <= b
 func lessThanOrEqualSign(a time.Time, b time.Time) bool {
 	return a.Before(b) || a.Equal(b)
 }
 
+// a == b
 func equalSign(a time.Time, b time.Time) bool {
 	return a.Equal(b)
 }
 
+// a > b
 func greaterThanSign(a time.Time, b time.Time) bool {
 	return a.After(b)
 }
 
+// a >= b
 func greaterThanOrEqualSign(a time.Time, b time.Time) bool {
 	return a.After(b) || a.Equal(b)
 }
